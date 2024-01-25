c554a64acfd49da4927a82ce92d6a8700796b82b
GabrielNilsson <Gabriel.Nilsson@elev.ga.lbs.se>
2024-01-25 10:04:32 +0100

    Fixat Uppgradering

--- Assets/Scripts/BananaClick.cs
+++ Assets/Scripts/BananaClick.cs
@@ -5,16 +5,49 @@ using UnityEngine.UI;
 
 public class BananaClick : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int costAmount1;
+    [SerializeField] int costAmount2;
 
-        public TMPro.TextMeshProUGUI ClicksTotalText;
+    int clickMultiplier;
+    int amountOfMonkeys;
+
+    public TMPro.TextMeshProUGUI ClicksTotalText;
 
     float TotalClicks;
 
-    public void AddClicks()
+    private void Start()
     {
-        TotalClicks++;
+        clickMultiplier = 0;
+        amountOfMonkeys = 0;
+    }
+    private void Update()
+    {
         ClicksTotalText.text = TotalClicks.ToString("0");
     }
-    
+    public void AddClicks()
+    {
+        TotalClicks += 1 + clickMultiplier;
+    }
+    public void ApUpgradering()
+    {
+        if (TotalClicks >= costAmount1)
+        {
+            float currentClicks = TotalClicks;
+            amountOfMonkeys++;
+            TotalClicks = currentClicks - costAmount1;
+            costAmount1 *= 2;
+        }
+    }
+
+    public void FördubblingsUppgradering()
+    {
+        if (TotalClicks >= costAmount2)
+        {
+            float currentClicks = TotalClicks;
+            clickMultiplier++;
+            TotalClicks = currentClicks - costAmount2;
+            costAmount2 *= 2;
+        }
+    }
+
 }
